594c981aaeaa34f90cd490c4394176766ef1591e
Edyta Wiśniewska <eedyta.wisniewska@gmail.com>
2021-08-03 18:27:56 +0200

    After first class 0731

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from datetime import datetime
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    #on delete.cascade - jak usuniemy question powyższe to kaskadowo usunie się w choice
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
